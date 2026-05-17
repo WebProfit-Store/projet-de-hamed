44e63cd3d1543d5c933215e6c8b69a32be012b8f
WebProfit-Store <diabyhamed770@gmail.com>
2026-05-17 22:17:22 +0000

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <header style={{padding:"20px",borderBottom:"1px solid #eee"}}>
+      <div className="container">
+        <h1>Hamed Store</h1>
+      </div>
+    </header>
+  );
+}
