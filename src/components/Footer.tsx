2e92bacdea39544bcd955f8b525d3993dd53697e
WebProfit-Store <diabyhamed770@gmail.com>
2026-05-17 22:17:53 +0000

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer style={{padding:"20px",borderTop:"1px solid #eee"}}>
+      <div className="container">
+        <p>© Hamed Store</p>
+      </div>
+    </footer>
+  );
+}
