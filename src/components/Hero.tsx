72b450b697d3d074f94b8a0f6a6d77adb042ffe9
WebProfit-Store <diabyhamed770@gmail.com>
2026-05-17 22:18:26 +0000

    Create Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <section style={{padding:"100px 20px"}}>
+      <div className="container">
+        <h2 style={{fontSize:"60px"}}>
+          Ecommerce IA Premium
+        </h2>
+
+        <p style={{marginTop:"20px"}}>
+          Plateforme intelligente nouvelle génération.
+        </p>
+      </div>
+    </section>
+  );
+}
